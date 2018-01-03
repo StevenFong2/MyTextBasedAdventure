79f3187d76f67347191901613ec1f34d20023dc7
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2018-01-03 09:07:49 -0500

    NUH NUH NUH NUH BATMAN

--- TheAmazingJourneyofSteven/src/MarvelvDC.java
+++ TheAmazingJourneyofSteven/src/MarvelvDC.java
@@ -1,5 +1,6 @@
 //Saurabh Bansal
 import java.util.Scanner;
+
 public class MarvelvDC extends Room
 {
 	Scanner in = new Scanner (System.in);
@@ -9,8 +10,6 @@ public class MarvelvDC extends Room
 	{
 		super(x,y);
 	}
-	
-
 	public void enterRoom(Person x) 
 	{
 		// TODO Auto-generated method stub
@@ -31,6 +30,116 @@ public class MarvelvDC extends Room
 		{
 			System.out.println("You have a choice to be a superhero. Each hero has their own story.");
 			System.out.println("Heroes: Batman, Superman, Wonder Woman, Cyborg, Green lantern");
+			if (statement.length() == 0)
+			{
+				Runner.gameOff();
+			}
+			else if (findKeyword (statement, "batman") >= 0)
+			{
+				System.out.println("You are Bruce Wayne. Your parents were murdered in front of you, and now you have been "
+						+ "in Nanda Parbat training with Rha's Al Ghul. You are his most experienced fighter");
+				System.out.println("Rha's Al Ghul has given you the opportunity to be his successor. Do you choose to accept?");
+				if (findKeyword(statement, "yes") >= 0)
+				{
+					System.out.println ("You are now the successor of the most elite group of Assassins, League of Assassins,"
+							+ " known to mankind."
+							+ "Gotham (your home city) has fallen prey to corruption; What would you do?"
+							+ "Use the assassins, destroy the city and rebuild, or will you just not care.");
+					if (findKeyword(statement, "assassins") >= 0)
+					{
+						System.out.println("You have killed all known corrupt officials. Sadly, one of them was your "
+								+ "girlfriend who you loved with all your heart and soul. You now will live the rest of your life"
+								+ " knowing you killed your girlfriend, but as the leader of the League of Assassins.");
+					}
+					else if (findKeyword (statement, "destroy") >= 0)
+					{
+						System.out.println("Jesus Christ! You have burnt Gotham to the ground. EVERYONE IS DEAD."
+								+ " I can't believe you think of yourself as a hero. Some people don't like the fact"
+								+ " you destroyed Gotham. They have come to attack you at Nanda Parbat. Will you defend, or "
+								+ "kill them first?");
+						if (findKeyword (statement, "defend") >= 0)
+						{
+							System.out.println("You have defended, but at a cost. You died while defending.");
+							Runner.gameOff();
+						}
+						else 
+						{
+							System.out.println("You are a ruthless killer. Go on and PLZ DONT KILL ME FOR HOLDING YOU! I have a wife and child! "
+									+ "My wife's name is Microsoft and my child is XBOX. He has truly grown!");
+						}
+					}
+					else
+					{
+						System.out.println("Noice, enjoy your life with the treasure and power you have. You will sadly be stuck in "
+								+ "the game forever because we love you that much.");
+						Runner.gameOff();
+					}
+				}
+				else
+				{
+					System.out.println("You have chosen the path of Batman. There comes a time in your life where you "
+							+ "meet Rha's Al Ghul's daughter (Talia Al Ghul). You have a child with her, but she takes the child to Nanda Parbat."
+							+ " He becomes a ruthless killer, but an attack leads Talia to bring the son to you."
+							+ " Your son is an absolute killer. Are you willing to teach him the ways of Batman?");
+					if (findKeyword (statement, "yes") >= 0)
+					{
+						System.out.println ("Patiently, you got your son to learn to think first and then kill."
+								+ " He has now worn the mantle of Robin, and you two work together to fight crime.");
+					}
+					else 
+					{
+						System.out.println("He kills you for not being there for him.");
+						Runner.gameOff();
+					}
+				}
+			}
+			else if (findKeyword(statement, "super") >= 0)
+			{
+				System.out.println("You are the Kryptonite (god) sent from a spaceship to keep you safe from the evil regime!"
+						+ "You have lived a life on a farm and you are the STRONGEST man alive!"
+						+ "You like a girl named Lois Lane! She ");
+			}
+			else if (findKeyword(statement, "wonder") >= 0)
+			{
+				
+			}
+			else if (findKeyword (statement, "cyb") >= 0)
+			{
+				
+			}
+			else //Green Lantern
+			{
+				
+			}
+		}
+		else if (findKeyword (statement, "marvel")>=0)
+		{
+			System.out.println("You have a choice to be a superhero. Each hero has their own story.");
+			System.out.println("Heroes: Iron Man, Hulk, Black Widow, Captain America, or Black Panther");
+			if (statement.length() == 0)
+			{
+				Runner.gameOff();
+			}
+			else if (findKeyword (statement, "iron") >= 0)
+			{
+				
+			}
+			else if (findKeyword(statement, "hulk") >= 0)
+			{
+				
+			}
+			else if (findKeyword(statement, "widow") >= 0)
+			{
+				
+			}
+			else if (findKeyword (statement, "america") >= 0)
+			{
+				
+			}
+			else //Black Panther
+			{
+				
+			}
 		}
 		else
 		{
