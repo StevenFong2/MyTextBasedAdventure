3c9856d603a7fe8f4b3e124772e02482b37d38de
Mamadou Diallo <Mamadou Diallo@Xarin.home>
2018-01-02 23:01:54 -0500

    Completed Riddle room's 5 possible riddle

--- TheAmazingJourneyofSteven/src/LionRiddle.java
+++ TheAmazingJourneyofSteven/src/LionRiddle.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class LionRiddle extends Room {
 
 	
@@ -6,6 +7,7 @@ public class LionRiddle extends Room {
 		super(x, y);
 	}
 	public void enterRoom(Person x) {
+		Random R = new Random();
 		occupant = x;
 		x.setx(this.x);
 		x.sety(this.y);
@@ -14,8 +16,48 @@ public class LionRiddle extends Room {
 				+ " you see a stone lion with a tablet beneath its paws. The the tablet reads:");
 		
 		//Final Challenge Started
-		System.out.println("Subversive is I \nWith only one eye \nI have a bell inside \nI am a way to be free \nAnd created by someone unhappy \nAt the end, a feline resides \nWhat am I?"
-				+ "");
+		int riddlevalue = R.nextInt(5) + 1;
+		if(riddlevalue == 1)
+		{
+		System.out.println("Subversive is I "
+				+ "\nWith only one eye "
+				+ "\nI have a bell inside"
+				+ " \nI am a way to be free"
+				+ " \nAnd created by someone unhappy "
+				+ "\nAt the end, a feline resides");
+		}
+		if(riddlevalue == 2)
+		{
+			System.out.println("He comes by bedsides, icy bridges,"
+					+ "\nbattlefronts, and crumbling ridges."
+					+ "\nWhen he comes, he comes alone,"
+					+ "\ntaps a shoulder, then is gone.");
+		}
+		if(riddlevalue == 3)
+		{
+			System.out.println("I'm a word, six letters long,"
+					+ "\nI sometimes enter with a gong."
+					+ "\nAll in order from A to Z,"
+					+ "\nI start with the letter B.");
+		}
+		if(riddlevalue == 4)
+		{
+			System.out.print("I eat, I live"
+					+ "\nI breath, I live"
+					+ "\nI drink, I die");
+		}
+		if(riddlevalue ==5)
+		{
+			System.out.println("A crawling contradiction I aim to create"
+					+ "\nMy antithesis, my ideal state"
+					+ "\nlacks the very thing I so often make"
+					+ "\nAn example of hypocricy of the gods above,"
+					+ "\nI breed hate in the name of love"
+					+ "\nWhether it be a country or city"
+					+ "\nPatience and Time will always win me"
+					+ "\nWith nothing but a sign of ink, I come to life upon request"
+					+ "\nAnd in my final breathe, I determine not who's right, but who is left" );
+		}
 	}
 	
 	public void print()
